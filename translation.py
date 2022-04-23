47f92ddd48a7dda5a3167b9db5e865a8ead679c6
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2022-04-23 14:05:41 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -10,21 +10,21 @@ URL | filename | username | password"""
     DOWNLOAD_START = "📥Downloading..."
     UPLOAD_START = "📤Uploading..."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using me \n\n<b>Join @TMWAD For More UsefUl Bots Like Me </b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@TMWAD"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using me \n\n<b>Join @OMG_info For More UsefUl Bots Like Me </b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@OMG_info"
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     CUSTOM_CAPTION_UL_FILE = "{}"
     NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
     ABOUT_MSG = """ Something About Me :
     
-   ☞My Name  : All Url Uploader Bot
+   ☞My Name  : URL X Bot
 
-   ☞Updates  : @TMWAD    
+   ☞Updates  : @OMG_info    
 
    ☞Language : Python3
 
-   ☞Library  : <a href="https://docs.pyrogram.org/">Pyrogram 1.0.7</a>"""
+   ☞Library  : <a href="https://t.me/nexleech/">leech group</a>"""
     HELP_USER = """Please Follow These steps!
     
 1. Send url (example.domain/File.mp4 | New Filename.mp4).
@@ -35,7 +35,7 @@ URL | filename | username | password"""
    Video  - Give File as video without Screenshots
    File   - Give File without Screenshots
 
-If bot didn't respond, Ask Here @TMWAD"""
+If bot didn't respond, Ask Here @nexleech"""
     REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
 You can use /rename command after receiving file to rename it with custom thumbnail support.
